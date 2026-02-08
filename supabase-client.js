22e117091a26f8d40c4a858d340946140b0957a4
szcmawoXIII <107519069+szcmawoXIII@users.noreply.github.com>
2026-02-08 16:19:39 +0300

    supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+export const supabase = createClient(
+    "https://fezlfobvavcxpwzovsoz.supabase.co",
+    "sb_publishable_HjeSTZJOE2JEKBfuG1BxAQ_8oj30LvD"
+);
